f8040793c4c3259416df22104ebb8c55f9a8c6a5
mym0404 <mym0404@gmail.com>
2022-04-24 19:21:30 +0900

    Add Leak canary plugin

--- core/src/main/java/happy/mjstudio/core/presentation/util/FlipperUtil.kt
+++ core/src/main/java/happy/mjstudio/core/presentation/util/FlipperUtil.kt
@@ -5,11 +5,14 @@ import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.leakcanary2.FlipperLeakListener
+import com.facebook.flipper.plugins.leakcanary2.LeakCanary2FlipperPlugin
 import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
 import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 import com.facebook.soloader.SoLoader
 import happy.mjstudio.core.BuildConfig
+import leakcanary.LeakCanary
 import okhttp3.OkHttpClient
 
 object FlipperUtil {
@@ -17,11 +20,16 @@ object FlipperUtil {
 
     fun init(app: Application) {
         SoLoader.init(app, false)
+        LeakCanary.config = LeakCanary.config.copy(
+            onHeapAnalyzedListener = FlipperLeakListener()
+        )
+
         if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(app)) {
             val client = AndroidFlipperClient.getInstance(app)
             client.addPlugin(InspectorFlipperPlugin(app, DescriptorMapping.withDefaults()))
             client.addPlugin(flipperNetworkPlugin)
             client.addPlugin(SharedPreferencesFlipperPlugin(app, "hey"))
+            client.addPlugin(LeakCanary2FlipperPlugin())
             client.start()
         }
     }
